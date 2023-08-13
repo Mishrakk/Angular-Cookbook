32f87f6e428bfb146388fb9041d095004791ae82
Szymon Murawski <szymon.murawski@gmail.com>
2023-08-13 23:00:14 +0200

    feat: configure injector with DI token

--- chapter03/start_here/ng-di-token/src/app/classes/greeter.class.ts
+++ chapter03/start_here/ng-di-token/src/app/classes/greeter.class.ts
@@ -1,7 +1,14 @@
+import { InjectionToken } from '@angular/core';
 import { User } from '../interfaces/user.interface';
+
+export const GREETER = new InjectionToken('Greeter', {
+  providedIn: 'root',
+  factory: () => Greeter
+})
+
 export class Greeter implements User {
-  firstName;
-  lastName;
+  firstName: string;
+  lastName: string;
   constructor(user: User) {
     this.firstName = user.firstName;
     this.lastName = user.lastName;
