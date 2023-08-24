5b63cb02de2125dd0c5153bf10024fce8897ae8a
Szymon Murawski <szymon.murawski@gmail.com>
2023-08-24 21:48:32 +0200

    feat: add route animation

--- chapter04/start_here/route-animations/src/app/app.component.ts
+++ chapter04/start_here/route-animations/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { ROUTE_ANIMATION } from "./constants/animations";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
+  animations: [
+    ROUTE_ANIMATION
+  ]
 })
 export class AppComponent {
+  @ViewChild(RouterOutlet) routerOutlet;
   title = "ng-beta";
+
+  getRouteAnimationState(){
+    return this.routerOutlet?.activatedRouteData?.transitionState
+  }
 }
