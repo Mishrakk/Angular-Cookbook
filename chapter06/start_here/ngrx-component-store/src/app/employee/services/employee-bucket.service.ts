16bd61ad90b274c4706ff6615f5d6f7bfcb04e4f
Szymon Murawski <szymon.murawski@gmail.com>
2023-09-03 23:05:11 +0200

    feat: use ngrx component-store

--- chapter06/start_here/ngrx-component-store/src/app/employee/services/employee-bucket.service.ts
+++ chapter06/start_here/ngrx-component-store/src/app/employee/services/employee-bucket.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { IFruit } from 'src/app/interfaces/fruit.interface';
 import { BucketService } from 'src/app/services/bucket.service';
 
 @Injectable({
@@ -9,7 +10,8 @@ export class EmployeeBucketService extends BucketService {
     super();
   }
 
-  removeItem() {
-    alert('Employees can not delete items');
-  }
+  readonly removeItem = this.updater((state, _: IFruit) => {
+    alert('Employees cannot delete items');
+    return state;
+  }) 
 }
