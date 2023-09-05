354710202c51911844df7c47ea15a0ac02a64390
Szymon Murawski <szymon.murawski@gmail.com>
2023-09-05 21:02:08 +0200

    feat: use @ngrx/router-store

--- chapter06/start_here/ngrx-router-store/src/app/store/app.selectors.ts
+++ chapter06/start_here/ngrx-router-store/src/app/store/app.selectors.ts
@@ -1,6 +1,7 @@
 import { createSelector, createFeatureSelector, select } from '@ngrx/store';
 import { IUser } from '../core/interfaces/user.interface';
 import { AppState } from './app.reducer';
+import { RouterReducerState, getSelectors } from '@ngrx/router-store';
 
 export const selectApp = createFeatureSelector<AppState>('app');
 
@@ -9,18 +10,20 @@ export const selectUsers = createSelector(
   (state: AppState) => state.users
 );
 
-export const selectCurrentUser = (uuid) => createSelector(
+export const selectRouter = createFeatureSelector<AppState, RouterReducerState<any>>('router');
+
+const { selectRouteParam } = getSelectors(selectRouter);
+
+export const selectUserUUID = selectRouteParam('uuid');
+
+export const selectCurrentUser = createSelector(
+  selectUserUUID,
   selectUsers,
-  (users: IUser[]) => users ? users.find(user => {
-    return user.login.uuid === uuid;
-  }) : null
+  (uuid, users: IUser[]) => users?.find(user => {return user.login.uuid === uuid;})
 );
 
-export const selectSimilarUsers = (uuid) => createSelector(
+export const selectSimilarUsers = createSelector(
+  selectUserUUID,
   selectUsers,
-  (users: IUser[]) => users ? users.filter(user => {
-    return user.login.uuid !== uuid;
-  }): null
+  (uuid, users: IUser[]) => users?.filter(user => {return user.login.uuid !== uuid;})
 );
-
-
