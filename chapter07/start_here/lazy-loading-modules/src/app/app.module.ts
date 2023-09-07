e801f8f18050c614853d2ec23b9f974e0dc89da9
Szymon Murawski <szymon.murawski@gmail.com>
2023-09-07 22:02:01 +0200

    feast: use lazy loading for modules

--- chapter07/start_here/lazy-loading-modules/src/app/app.module.ts
+++ chapter07/start_here/lazy-loading-modules/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
-import { HomeModule } from './home/home.module';
-import { AboutModule } from './about/about.module';
 
 @NgModule({
   declarations: [
@@ -15,8 +13,6 @@ import { AboutModule } from './about/about.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule,
-    AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
