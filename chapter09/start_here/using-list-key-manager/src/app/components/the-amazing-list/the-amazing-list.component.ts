4d0f5273b1aa2e6dca7569c0015c17ca84133c4c
Szymon Murawski <szymon.murawski@gmail.com>
2023-09-16 22:44:05 +0200

    feat: add keyboard navigation for lists

--- chapter09/start_here/using-list-key-manager/src/app/components/the-amazing-list/the-amazing-list.component.ts
+++ chapter09/start_here/using-list-key-manager/src/app/components/the-amazing-list/the-amazing-list.component.ts
@@ -1,16 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { FocusKeyManager } from '@angular/cdk/a11y';
+import { AfterViewInit, Component, HostListener, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { AppUserCard } from 'src/interfaces/app-user-card.interface';
+import { TheAmazingListItemComponent } from '../the-amazing-list-item/the-amazing-list-item.component';
 
 @Component({
   selector: 'app-the-amazing-list',
   templateUrl: './the-amazing-list.component.html',
-  styleUrls: ['./the-amazing-list.component.scss']
+  styleUrls: ['./the-amazing-list.component.scss'],
+  host: { role: 'list' },
 })
-export class TheAmazingListComponent implements OnInit {
+export class TheAmazingListComponent implements OnInit, AfterViewInit {
   @Input() listItems: Partial<AppUserCard>[] = [];
+  @ViewChildren(TheAmazingListItemComponent) listItemsElements: QueryList<TheAmazingListItemComponent>;
+  @HostListener('window:keydown', ['$event'])
+  onKeydown(event) {
+    this.listKeyManager.onKeydown(event);
+  }
+  private listKeyManager: FocusKeyManager<TheAmazingListItemComponent>;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+      this.listKeyManager = new FocusKeyManager(this.listItemsElements);
   }
 
 }
