56a9045806e4e9fff168c6f33027505ea84d76e5
Szymon Murawski <szymon.murawski@gmail.com>
2023-09-19 23:17:20 +0200

    feat: use CdkStepper to create a game

---
+++ chapter09/start_here/using-cdk-stepper/src/app/components/game-stepper/game-stepper.component.ts
@@ -0,0 +1,12 @@
+import { CdkStepper } from '@angular/cdk/stepper';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-stepper',
+  templateUrl: './game-stepper.component.html',
+  styleUrls: ['./game-stepper.component.scss'],
+  providers: [{provide: CdkStepper, useExisting: GameStepperComponent }]
+})
+export class GameStepperComponent extends CdkStepper {
+
+}
