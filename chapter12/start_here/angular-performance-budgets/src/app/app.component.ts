d7491f6b3e8950df3eda57624694e62d6c9cc2d0
Szymon Murawski <szymon.murawski@gmail.com>
2023-09-26 21:29:37 +0200

    feat: define performance budget

--- chapter12/start_here/angular-performance-budgets/src/app/app.component.ts
+++ chapter12/start_here/angular-performance-budgets/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { Scene } from 'three';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,10 @@ import { AuthService } from './services/auth.service';
 export class AppComponent {
   // DO NOT USE THE CODE BELOW IN PRODUCTION
   // IT WILL CAUSE PERFORMANCE ISSUES
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(private auth: AuthService, private router: Router) {
+    const scene = new Scene();
+    console.log(format(new Date(), 'LLL do yyyy'));
+  }
 
   get isLoggedIn() {
     return this.auth.isLoggedIn();
